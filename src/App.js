8ccdfbf090354706bdc2e9f1ab496fa9621a54c1
Milagros Garcia <miligarcia048@gmail.com>
2023-02-16 20:19:02 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import AddProject from './pages/AddProject';
-import ProjectDetail from './pages/ProjectDetail';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import AddProject from "./pages/AddProject";
+import ProjectDetail from "./pages/ProjectDetail";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import IsPrivate from './components/IsPrivate';
-import { Calendar } from 'react-calendar';
+import IsPrivate from "./components/IsPrivate";
+import { Calendar } from "react-calendar";
+import { useState, useContext } from "react";
+import { UserContext } from "../context/user.context";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   const { loggedUser } = useContext(UserContext);
@@ -16,11 +20,10 @@ function App() {
     <div className="App">
       <ToastContainer />
       {loggedUser && <Navbar />}
-
       <Routes>
-        <Route path={'/'} element={<Calendar/>}/>
-        <Route path={'/signup'} element={<Signup/>}/>
-        <Route path={'/login'} element={<Login/>}/>
+        <Route path={"/"} element={<Calendar />} />
+        <Route path={"/signup"} element={<Signup />} />
+        <Route path={"/login"} element={<Login />} />
       </Routes>
     </div>
   );
