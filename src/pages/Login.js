e348ee0ff443ba0b8b2b9b2cdcda79d73c39132c
Milagros Garcia <miligarcia048@gmail.com>
2023-02-21 00:28:36 +0000

    login styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,8 +5,9 @@ import { useState, useContext } from "react";
 import { UserContext } from "../context/user.context";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Form";
-import Col from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -42,41 +43,56 @@ function Login() {
   }
 
   return (
-    <div className="w-75 justify-content-md-center">
-      <Form onSubmit={handleSubmitForm}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label htmlFor="email">Email address</Form.Label>
-          <Form.Control
-            placeholder="Enter email"
-            id="email"
-            type="text"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <Container fluid className="login-page">
+      <Row>
+        <Col className="login-form">
+          <div className="login-form-text">
+            {" "}
+            <h3>Sign in</h3>
+            <p>
+              If you don’t have an account register You can{" "}
+              <Link to={"/signup"}>Register here !</Link>{" "}
+            </p>
+          </div>
+          <Form onSubmit={handleSubmitForm}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label htmlFor="email">Email address</Form.Label>
+              <Form.Control
+                placeholder="Enter email"
+                id="email"
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-        <Link to={"/signup"}>Sign Up</Link>
-      </Form>
-    </div>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label htmlFor="password">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Col>
+        <Col className="login-left">
+          <img src="illustration.svg" />
+          <h3>Welcome to Simplify</h3>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
