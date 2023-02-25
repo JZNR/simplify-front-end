470b30dd71d5b8ac32b9ab88f70498a17f65a03d
Milagros Garcia <miligarcia048@gmail.com>
2023-02-25 14:03:37 +0000

    edit modal

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -2,13 +2,15 @@ import React, { useEffect } from "react";
 import CustomCalendar from ".././components/CustomCalendar";
 import { useState } from "react";
 import AddEventModal from ".././components/AddEventModal";
+import EditEventModal from ".././components/EditEventModal";
 import { getEvents, deleteEvent } from "../api";
 
 function CalendarPage() {
   const [modalShow, setModalShow] = useState(false);
+  const [modalEditShow, setModalEditShow] = useState(false);
   const [eventDate, seteventDate] = useState();
   const [events, setEvents] = useState("");
-
+  const [editEventInfo, setEditEventInfo] = useState("");
   async function getAllEvents() {
     const response = await getEvents();
     setEvents(response.data);
@@ -19,10 +21,20 @@ function CalendarPage() {
     seteventDate(arg.dateStr);
   };
 
-  function editEvent(e) {
-    console.log("eventclick", e.event._def);
-    deleteEvent(e.event._def.extendedProps._id);
+  function handledeleteEvent(e) {
+    console.log("delete event", e.event._def.extendedProps._id);
+    const eventId = e.event._def.extendedProps._id;
+    deleteEvent(eventId);
+    setModalEditShow(false);
+
     getAllEvents();
+  }
+
+  function editEvent(e) {
+    console.log("event click", e.event._def.extendedProps._id);
+    setModalEditShow(true);
+    getAllEvents();
+    setEditEventInfo(e.event._def);
   }
 
   useEffect(() => {
@@ -44,7 +56,17 @@ function CalendarPage() {
           show={modalShow}
           onHide={() => setModalShow(false)}
           setEvents={setEvents}
+        />
+      )}
+      {modalEditShow && (
+        <EditEventModal
+          eventDate={eventDate}
+          show={modalEditShow}
+          onHide={() => setModalEditShow(false)}
+          setEvents={setEvents}
           editEvent={editEvent}
+          handledeleteEvent={handledeleteEvent}
+          editEventInfo={editEventInfo}
         />
       )}
     </>
